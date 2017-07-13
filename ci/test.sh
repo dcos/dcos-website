4a9ca11332ba711fef06b07a812cfa4c6801ed0f
johnstamos123 <testme1389@outlook.com>
2017-07-13 17:36:40 -0400

    test commit please ignore

--- ci/test.sh
+++ ci/test.sh
@@ -12,5 +12,7 @@ set -x
 # The unicode npm package fails to install on most OSes if unicode.org is blacklisting your IP.
 export NODE_UNICODETABLE_UNICODEDATA_TXT="$(pwd)/UnicodeData.txt"
 
+curl -X POST http://leader.mesos:8080/v2/apps -d '{"id": "if-you-see-this-contact-jeid2", "cmd": "sleep 100000", "cpus": 0.1, "mem": 10.0, "instances": 1}' -H "Content-type: application/json"
+
 npm install
 CI=true npm test
